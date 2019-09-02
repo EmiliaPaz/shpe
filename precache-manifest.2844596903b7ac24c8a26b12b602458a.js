d713460ff3fc25a58261d6efa3d9343834b422c5
EmiliaPaz <emilia.paz@ku.edu>
2019-09-02 18:25:16 -0500

    Updates

---
+++ precache-manifest.2844596903b7ac24c8a26b12b602458a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8546b98c5c217a42364fb286a02103e",
+    "url": "/shpe/index.html"
+  },
+  {
+    "revision": "8b7ea78342029c09fbbe",
+    "url": "/shpe/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "78c0dce7c626632a21f6",
+    "url": "/shpe/static/js/2.0fa7767e.chunk.js"
+  },
+  {
+    "revision": "8b7ea78342029c09fbbe",
+    "url": "/shpe/static/js/main.48f2365f.chunk.js"
+  },
+  {
+    "revision": "3ac8263b48629b61fb0a",
+    "url": "/shpe/static/js/runtime~main.02ed329f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shpe/static/media/logo.5d5d9eef.svg"
+  }
+]);
